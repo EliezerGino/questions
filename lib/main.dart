3dc85ca138482537720996e9e7494f8e7135ee36
Eliezer Castro <eliezergino@gmail.com>
2020-03-08 18:31:45 -0300

    app

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import './questions.dart';
+import './resposta.dart';
+
+main() => runApp(QuestionsApp());
+
+class _PerguntaAppState extends State<QuestionsApp>{
+  var perguntaSelecionada = 0;
+
+  void _responder(){
+    setState(() {
+      perguntaSelecionada++;
+    });
+    print(perguntaSelecionada);
+  }
+  @override
+  Widget build(BuildContext context){
+     final List <Map<String, Object>> perguntas = [
+      {
+        'texto':'Qual a sua cor favorita?',
+        'resposta': ['Preto', 'Vermelho', 'Verde', 'Branco',]
+      },
+      {
+        'texto': 'Qual a sua cor favorita?',
+        'resposta': ['Coelho', 'Cobra', 'Elefante', 'Leão',]
+      },
+      {
+        'texto': 'Qual é o seu instrutor vavorito?',
+        'resposta': ['Maria', 'Joãso', 'Leo', 'Pedro',]
+      }
+    ];
+
+    List<String> respostas = perguntas[perguntaSelecionada]['resposta'];
+    List<Widget> widgets = respostas
+      .map((t) => Resposta(t, _responder))
+      .toList();
+
+    return MaterialApp(
+      home:  Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Questions(perguntas[perguntaSelecionada]['texto']),
+            ...widgets, 
+          ],
+        ),
+      )
+    );
+  }
+}
+ 
+class QuestionsApp extends StatefulWidget{
+    _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }
+}
